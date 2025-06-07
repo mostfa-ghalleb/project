8ac3264ae6b2c99a75845fb215150a4926b90d89
mostfa-ghalleb <mostfaghalleb9@gmail.com>
2025-06-07 01:00:42 +0000

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    mdx()
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark',
+      wrap: true
+    }
+  }
+});
